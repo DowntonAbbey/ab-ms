7b549300b66ac3035155ec8bd6406a332ce0ac43
houshangkun <1318680455@qq.com>
2020-02-04 19:13:09 +0800

    项目启动

---
+++ ab-mock/src/main/java/com/bank/abmock/AbMockApplication.java
@@ -0,0 +1,13 @@
+package com.bank.abmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbMockApplication.class, args);
+    }
+
+}
